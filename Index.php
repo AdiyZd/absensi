080e81e4080bd5cf345e962f15c9cd61eeba2cb1
AdiyZd <154992866+AdiyZd@users.noreply.github.com>
2024-08-31 08:57:34 +0700

    Index.php

--- Index.php
+++ Index.php
@@ -3,21 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link>
+    <link rel="icon" href="Aset/foto.jpg">
+   <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Biodata</title>
-    <style>
-        /* Gaya CSS bisa ditambahkan di sini */
-    </style>
+    <title>ABSENSI SANDIMEDIA</title>
 </head>
 <body>
-  <header>
-    <div class="biodata">
-      <!-- navbar -->
-      <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
+  <div class="nav-bar" >
+    <!-- navbar -->
+    <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
   <div class="bg-dark p-4">
-    <h5 class="text-body-emphasis h4">Biodata Adi</h5>
-    <span class="text-body-secondary">Berita Biodata Adi!</span>
+    <h5 class="text-body-emphasis h4">Sandi komputer</h5>
+    <span class="text-body-secondary">Websaite absensi di buat oleh Adiy</span>
   </div>
 </div>
 <nav class="navbar navbar-dark bg-dark">
@@ -27,24 +24,42 @@
     </button>
   </div>
 </nav>
-    </div>
-    
-    <!-- Informasi biodata-->
-    <div class="card mb-3" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="aset_foto/saya.png" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <h5 class="card-title">Adiy</h5>
-        <p class="card-text">Biodata ini di buat dengan tujuan agar orang bisa melihat saya dengan jelas!</p>
-        <p class="card-text"><small class="text-body-secondary">saya adalah programer newbi!</small></p>
-      </div>
-    </div>
-  </div>
+    <!-- navbar end -->
+    <div class="absensi">
+      <div class="input-group flex-nowrap">
+        <span class="input-group-text" id="addon-wrapping">@</span>
+        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+        </div>
+        </div>
+  <!-- akhir dari input -->
+  <!-- ijin -->
+  <h2 class="Adiy_h2">ABSENSI SANDIMEDIA</h2>
+  <hr>
+  <!-- awak cek box -->
+  <div class="cb">
+    <div class="btn-group" role="group" aria-label="Basic outlined example">
+  <button type="button" class="btn btn-outline-primary">MASUK</button>
+  <button type="button" class="btn btn-outline-primary">SETENGAH HARI</button>
+  <button type="button" class="btn btn-outline-primary">SAKIT</button>
 </div>
-  </header>
+  </div>
+  <hr class="hr_2">
+  </div>
+  <!-- menu button -->
+  <div class="d-grid gap-2">
+  <button class="btn btn-primary" type="button" onclick="main()">KIRIM ABSENSI</button>
+</div>
+    <!-- ini adalah akhir -->
+<footer>
+        <p>&copy; 2024 Your Company. All rights reserved.</p>
+        <p>
+            Follow us on 
+            <a href="https://www.facebook.com" target="100035408843000">Facebook</a>, 
+            <a href="https://www.twitter.com" target="_blank">Twitter</a>, and 
+            <a href="https://www.instagram.com" target="xyd_adiy">Instagram</a>.
+        </p>
+    </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <script>Main.js</script>
 </body>
 </html>
