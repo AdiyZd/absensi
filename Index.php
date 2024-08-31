4ed3e955b81c5395dff7dc1fae6888f7131ac0b7
AdiyZd <154992866+AdiyZd@users.noreply.github.com>
2024-08-31 08:54:19 +0700

    Index.php

---
+++ Index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Biodata</title>
+    <style>
+        /* Gaya CSS bisa ditambahkan di sini */
+    </style>
+</head>
+<body>
+  <header>
+    <div class="biodata">
+      <!-- navbar -->
+      <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
+  <div class="bg-dark p-4">
+    <h5 class="text-body-emphasis h4">Biodata Adi</h5>
+    <span class="text-body-secondary">Berita Biodata Adi!</span>
+  </div>
+</div>
+<nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+  </div>
+</nav>
+    </div>
+    
+    <!-- Informasi biodata-->
+    <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="aset_foto/saya.png" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Adiy</h5>
+        <p class="card-text">Biodata ini di buat dengan tujuan agar orang bisa melihat saya dengan jelas!</p>
+        <p class="card-text"><small class="text-body-secondary">saya adalah programer newbi!</small></p>
+      </div>
+    </div>
+  </div>
+</div>
+  </header>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
